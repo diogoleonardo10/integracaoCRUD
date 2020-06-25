fb2ea46388b95d042fb3d5e93f6afefda6758759
diogoleonardo10 <diogoleonardo10@gmail.com>
2020-06-24 21:56:45 -0300

    microservico adiconado

---
+++ MicroServico/ConsultaProcesso/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace ConsultaProcesso
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new ServicoConsultaProcesso()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ MicroServico/ConsultaProcesso/ServicoConsultaProcesso.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsultaProcesso
+{
+    public partial class ServicoConsultaProcesso : ServiceBase
+    {
+        public ServicoConsultaProcesso()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //crio uma nova thread com sobrecarga chamando o metodo que eu quero que microservico execute
+            Thread thread = new Thread(new ThreadStart(ConsultaProcesso));
+            thread.Start();
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        protected void ConsultaProcesso()
+        {
+            // enquanto for verdadeiro para ficar sempre em execução
+            while (true)
+            {
+                //consulta banco de dados ou API
+                //Verificar movimento do processo por data
+                //movimento enviar e-mail
+            }
+
+        }
+    }
+}
